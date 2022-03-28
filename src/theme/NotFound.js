a315e1f6fcf62bc9300472f293c0a1242d5f1ecb
Raman Gupta <ramanxg@gmail.com>
2022-03-28 11:10:09 -0700

    Added support for 'window' variable

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import NotFound from '@theme-original/NotFound';
-import redirects from '@site/static/redirects.json'
+import redirects from '@site/static/redirects.json';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function NotFoundWrapper(props) {
-  // Get the pathname and remove the leading /
-  // E.g. "/discord" -> "discord"
-  const pathname = window.location.pathname.substring(1);
+  return (
+    <BrowserOnly fallback={<NotFound {...props}/>}>
+      {() => {
+        // Get the pathname and remove the leading /
+        // E.g. "/discord" -> "discord"
+        const pathname = window.location.pathname.substring(1);
 
-  // If the path name is in the list of redirects,
-  // redirect to that location and return an empty component
-  if (pathname in redirects) {
-    window.location.replace(redirects[pathname])
-    return <></>
-  }
+        // If the path name is in the list of redirects,
+        // redirect to that location and return an empty component
+        if (pathname in redirects) {
+          window.location.replace(redirects[pathname])
+          return <></>
+        }
 
-  // Otherwise, return the origin 404 page
-  return <NotFound {...props} />
-}
+        // Otherwise, return the origin 404 page
+        return <NotFound {...props} />
+    }}
+    </BrowserOnly>
+  );
+};
+  
