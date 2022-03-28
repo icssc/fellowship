b3acbbc1c2cf530ed48a1ce1ff0a2895d2ce8667
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-03-28 01:16:16 -0700

    Add redirects

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/"
-            disabled
+            to="/apply"
           >
             Apply
           </Link>
