29a1a2cb38e4290f3843b1ac2914a31ba88cedc8
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-03-28 13:24:24 -0700

    revert apply link overwrite

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/"
-            disabled
+            to="/apply"
+            
           >
             Apply
           </Link>
