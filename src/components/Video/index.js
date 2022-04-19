3cd883560a2caf92064ede3d6f0d483c9aa21603
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-04-19 03:36:39 -0700

    Change video url prop to src

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function Video(props) {
-    const { url } = props;
+    const { src } = props;
 
     return(
         <div style={{width:"560px", maxWidth: "100%"}}>
-            <iframe width="100%" height="315" src={url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
+            <iframe width="100%" height="315" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
         </div>   
     ) 
 }
